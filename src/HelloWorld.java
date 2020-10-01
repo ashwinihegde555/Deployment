b215609a337d8f5246ffe7f6d3fb140b21491e7c
Ashwini <Ashwini@DESKTOP-KKDK48C>
2020-10-01 09:56:01 +0530

    Hello world

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+	    int x=30;
+	 String firstname="Ashwini";
+	 String lastname="Hegde";
+	 String fullname= firstname + lastname;
+	System.out.println(fullname);
+	 String name="ashwini";
+	System.out.println("coming from");
+	System.out.println("  ");
+	   final float y= 2;
+	    System.out.println(x  + " " +  name + " " + y);
+	}
+}
